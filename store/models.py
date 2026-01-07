317e06d697eb5253ff8240a8d77e574ce711d0b6
prudhvi-code <prudhvitejakukkunuri@gmail.com>
2026-01-07 13:55:44 +0530

    Update

--- store/models.py
+++ store/models.py
@@ -11,7 +11,7 @@ class Products(models.Model):
     stock          =models.IntegerField()
     is_available   =models.BooleanField(default=True)
     category       =models.ForeignKey(Category,on_delete=models.CASCADE)
-    created_data   =models.DateTimeField(auto_now_add=True)
+    created_data  =models.DateTimeField(auto_now_add=True)
     modified_date  =models.DateTimeField(auto_now=True)
 
     def __str__(self):
@@ -20,4 +20,28 @@ class Products(models.Model):
     def get_url(self):
         return reverse('product_detail',args=[self.category.slug,self.slug])
     
+variation_category_choice=(
+    ('color','color'),
+    ('size','size')
+)
+    
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager,self).filter(variation_category='color',is_active=True) 
+    def sizes(self):
+        return super(VariationManager,self).filter(variation_category='size',is_active=True)
+
+
+class Variation(models.Model):
+    product=models.ForeignKey(Products,on_delete=models.CASCADE)
+    variation_category=models.CharField(max_length=100,choices=variation_category_choice)
+    variation_value=models.CharField(max_length=100)
+    is_active    =models.BooleanField(default=True)
+    created_data =models.DateTimeField(auto_now=True)
+
+    objects=VariationManager()
+    def __str__(self):
+        return self.variation_value
+
+
 
